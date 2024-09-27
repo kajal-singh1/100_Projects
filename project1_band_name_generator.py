7434e5a1b3f0f309bd07d377d1f8ca6407388413
Kajal Singh <ks9392656@gmail.com>
2024-09-27 15:31:25 +0530

    Band name generator

--- project1_band_name_generator.py
+++ project1_band_name_generator.py
@@ -4,10 +4,10 @@ pet = (input("what's your pet name?\n"))
 print("Your band name could be  "+city +" "+ pet )
 
 #Interactive Coding
-a = 3
-b = 7
-c = a
-a = b
-print("value of a =" , a)
-b = c
-print("value of b =", b)
+# a = 3
+# b = 7
+# c = a
+# a = b
+# print("value of a =" , a)
+# b = c
+# print("value of b =", b)
