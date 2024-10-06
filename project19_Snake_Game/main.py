f41de324890ff317b6151f74fbe68a5d334fa502
Kajal Singh <ks9392656@gmail.com>
2024-10-06 22:00:23 +0530

    Continuation of SNAKE GAME

--- project19_Snake_Game/main.py
+++ project19_Snake_Game/main.py
@@ -1,5 +1,7 @@
 from turtle import Screen
 from snake import Snake
+from food import Food
+from scoreboard import ScoreBoard
 import time
 
 screen = Screen()
@@ -9,6 +11,8 @@ screen.tracer(0)
 screen.setup(width=600, height=600)
 
 snake = Snake()
+food = Food()
+scoreboard = ScoreBoard()
 
 screen.listen()
 screen.onkey(snake.up,"Up")
@@ -19,9 +23,28 @@ screen.onkey(snake.right,"Right")
 game_is_on = True
 while game_is_on:
     screen.update()
-    time.sleep(0.3)
+    time.sleep(0.2)
 
     snake.move()
+
+    #Detects collision with food
+    if snake.head.distance(food) < 15:
+        food.refresh()
+        snake.extend()
+        scoreboard.increase_score()
+
+    #Detects collision with wall.
+    if snake.head.xcor() > 280 or snake.head.xcor() < -280 or snake.head.ycor() < -280 or snake.head.ycor() > 280:
+        game_is_on = False
+        scoreboard.game_over()
+
+    #Detects collision with tail
+    for segment in snake.segments[1:]:
+        if snake.head.distance(segment) < 10:
+             game_is_on = False
+             scoreboard.game_over()
+
+
+
     
-  
 screen.exitonclick()
--- project19_Snake_Game/snake.py
+++ project19_Snake_Game/snake.py
@@ -1,5 +1,5 @@
 from turtle import Turtle
-POSITION = [0, -20, -40, -60]
+STARTING_POSITION = [(0,0), (-20,0), (-40,0)]
 MOVE_DISTANCE = 20
 UP = 90
 DOWN = 270
@@ -14,12 +14,18 @@ class Snake:
         self.head = self.segments[0]
        
     def create_snake(self):
-        for tim in range(0, 3):
-            new_segment= Turtle(shape = ("square"))
-            new_segment.color("white")
-            new_segment.penup()
-            new_segment.goto(x= POSITION[tim] , y =0 )
-            self.segments.append(new_segment)
+        for position in STARTING_POSITION:
+            self.add_segment(position)
+            
+    def add_segment(self, position):
+        new_segment= Turtle(shape = ("square"))
+        new_segment.color("white")
+        new_segment.penup()
+        new_segment.goto(position )
+        self.segments.append(new_segment)
+
+    def extend(self):
+        self.add_segment(self.segments[-1].position())
 
     def move(self):
         for seg_num in range(len(self.segments)-1, 0, -1):
