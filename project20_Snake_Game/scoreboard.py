e6575693f7a266c76b90bc115b2a446378acc16b
Kajal Singh <ks9392656@gmail.com>
2024-10-09 21:01:27 +0530

    modifications

--- project20_Snake_Game/scoreboard.py
+++ project20_Snake_Game/scoreboard.py
@@ -8,6 +8,7 @@ class ScoreBoard(Turtle):
     def __init__(self):
         super().__init__()
         self.score = 0
+        self.high_score = 0
         self.color("white")
         self.penup()
         self.goto(0, 260)
@@ -16,13 +17,15 @@ class ScoreBoard(Turtle):
 
 
     def update_score(self):
-        self.write(f"Score: {self.score}" , False , align=ALIGNMENT, font= FONT)
+        self.clear()
+        self.write(f"Score: {self.score} High Score:{self.high_score}" , align=ALIGNMENT, font= FONT)
 
-    def game_over(self):
-        self.goto(0, 0)
-        self.write("GAME OVER" , False , align="center", font=('Arial', 24 , 'normal'))
+    def reset(self):
+        if self.score > self.high_score:
+            self.high_score = self.score
+        self.score = 0
+        self.update_score()
 
     def increase_score(self):
         self.score += 1
-        self.clear()
         self.update_score()
