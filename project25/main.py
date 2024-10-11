5a3e5d18966ac16cd28babe27c15c1e71124295a
Kajal Singh <ks9392656@gmail.com>
2024-10-11 22:47:20 +0530

    Added main.py

---
+++ project25/main.py
@@ -0,0 +1,22 @@
+# with open ("C:/Users/AJAY/Documents/sau_code/angela yu/100_Projects/project25/weather_data.csv") as new_data:
+#     data = new_data.readlines()
+#     print(data)
+
+import csv
+
+with open("C:/Users/AJAY/Documents/sau_code/angela yu/100_Projects/project25/weather_data.csv") as data_files:
+    data = csv.reader(data_files)
+    temperature = []
+    # temperature = csv.DictReader(data_files, delimiter = ',')
+    # columns = list(temperature)
+    # for column in columns:
+    #     print(column['temp'])
+    for row in data:
+        if row[1] != 'temp':
+            temperature.append(int(row[1]))
+    print(temperature)
+
+import pandas
+
+data = pandas.read_csv("C:/Users/AJAY/Documents/sau_code/angela yu/100_Projects/project25/weather_data.csv")
+print(data['temp'])
