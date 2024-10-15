78e8213a7cbfa52c1d339a2fc0891e54d246035f
Kajal Singh <ks9392656@gmail.com>
2024-10-15 12:12:26 +0530

    made changes in main.py

--- project27/main.py
+++ project27/main.py
@@ -1,25 +1,40 @@
 from tkinter import *
 
-def button_clicked():
-    print("I got clicked")
-    new_text = input.get()
-    label.config(text = new_text)
+def miles_to_km():
+    miles = float(input.get())
+    km = miles * 1.609
+    Km_result_label.config(text=f"{km}")
 
 #Creating a new window and configurations
 window = Tk()
-window.title("Widget Examples")
-window.minsize(width=500, height=500)
+window.title("Mile to Km Converter")
+# window.minsize(width=300, height=200)
+window.config(padx=20,pady=20)
+
+input = Entry( width=10)
+input.grid(column=1, row=0)
 
 #Labels
-label = Label(text="This is old text")
-label.config(text="New text")
-label.grid(column=0, row=0)
+miles_label = Label(text="Miles")
+miles_label.grid(column=2, row=0)
+
+is_equal_label = Label(text="is equal to")
+is_equal_label.grid(column=0, row=1)
+
+Km_result_label = Label(text="0")
+Km_result_label.grid(column=1, row=1)
+
+km_label = Label(text="kilometer")
+km_label.grid(column=2, row=1)
 
 #Button
-button = Button(text="Click Me", command=button_clicked)
+button = Button(text="Calculate", command=miles_to_km)
+button.grid(column=1,row=2)
 
-input = Entry(width=10)
-print(input.get())
+
+
+
+
 
 
 
