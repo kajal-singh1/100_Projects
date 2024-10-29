47e4d9641076f293db28bb1e2a607f77c65b4ad4
Kajal Singh <ks9392656@gmail.com>
2024-10-29 21:40:26 +0530

    added main.py

---
+++ project38_Workout_Tracking_through_GoogleSheets/main.py
@@ -0,0 +1,30 @@
+import requests
+
+GENDER = 'female'
+WEIGHT_KG = 53
+HEIGHT_CM = 146
+AGE = 22
+
+APP_ID = 'your_appid'
+API_KEY = 'your apikey	'
+
+exercise_endpoint = "https://trackapi.nutritionix.com/v2/natural/exercise"
+
+exercise_text = input("Tell me which exercises you did: ")
+
+headers = {
+    "x-app-id": APP_ID,
+    "x-app-key": API_KEY,
+}
+
+parameters = {
+    "query": exercise_text,
+    "gender": GENDER,
+    "weight_kg": WEIGHT_KG,
+    "height_cm": HEIGHT_CM,
+    "age": AGE
+}
+
+response = requests.post(exercise_endpoint, json=parameters, headers=headers)
+result = response.json()
+print(result)
