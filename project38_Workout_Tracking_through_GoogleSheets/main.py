d3ef9b96e9786f70f4a9322e8bde5eaeb385d350
Kajal Singh <ks9392656@gmail.com>
2024-10-30 19:23:16 +0530

    added changes in  main.py

--- project38_Workout_Tracking_through_GoogleSheets/main.py
+++ project38_Workout_Tracking_through_GoogleSheets/main.py
@@ -1,14 +1,16 @@
 import requests
+from datetime import datetime
 
 GENDER = 'female'
 WEIGHT_KG = 53
 HEIGHT_CM = 146
 AGE = 22
 
-APP_ID = 'your_appid'
-API_KEY = 'your apikey	'
+APP_ID = '5dab5cf0'
+API_KEY = '5294cf1d7a5c18d8bbdc05fe54391d4b'
 
 exercise_endpoint = "https://trackapi.nutritionix.com/v2/natural/exercise"
+sheet_endpoint = "https://api.sheety.co/fb15bd94c6a79690169ef25886ab6966/workoutTracking/workouts"
 
 exercise_text = input("Tell me which exercises you did: ")
 
@@ -28,3 +30,36 @@ parameters = {
 response = requests.post(exercise_endpoint, json=parameters, headers=headers)
 result = response.json()
 print(result)
+
+today_date = datetime.now().strftime("%d%m%Y")
+now_time = datetime.now().strftime("%X")
+
+for exercise in result["exercises"]:
+    sheet_inputs = {
+        "workout":{
+            "date": today_date,
+            "time": now_time,
+            "exercise": exercise["name"].title(),
+            "duration": exercise["duration_min"],
+            "calories": exercise["nf_calories"]
+        }
+    }
+    
+    sheet_response = requests.post(sheet_endpoint, json=sheet_inputs)
+
+    print(sheet_response.text)
+
+head = {
+    'Authorization': 'Basic bGFsaTphMzAxMHNqaGtsZ2o0NjMxMjM0bmhoY2ZkZmI='
+}
+
+sheet_response = requests.post(
+  sheet_endpoint, 
+  json=sheet_inputs, 
+#   headers=head 
+  auth=(
+       'lali',
+       'a3010sjhklgj4631234nhhcfdfb',
+    ),
+   
+)
